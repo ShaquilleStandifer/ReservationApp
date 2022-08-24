febd1c59926c26c0a581679bf7aae342a72f5c1f
Shaquille Standifer <shaquillestandifer@gmail.com>
2022-08-24 18:56:24 -0500

    completed

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL = 
+ const API_BASE_URL = //'https://reservations-app-backend.vercel.app'
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
  
  /**
